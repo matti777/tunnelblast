2d5e3e0b20144cfe2da86cab641da74de5026ad2
Matti Dahlbom <matti.dahlbom@qvik.fi>
2016-09-17 21:06:47 +0300

    Refactored input handling into Input.js

---
+++ client/html/js/Input.js
@@ -0,0 +1,65 @@
+// Define namespace
+var APP = APP || {};
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var intersectPlane; // Plane of intersect (= at myPaddle's z) for pointer events
+var intersectPoint = new THREE.Vector3();
+var intersectOffset = new THREE.Vector3();
+var draggingPaddle = false;
+
+APP.Input = function(renderer, camera, myPaddle) {
+  this.onMouseMove = function(event) {
+    event.preventDefault();
+
+    if (draggingPaddle) {
+      // Transform touch point coordinates into normalized device coordinates [-1,1]
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera(mouse, this.camera);
+
+      if (raycaster.ray.intersectPlane(intersectPlane, intersectPoint)) {
+        this.myPaddle.moveTo(intersectPoint.sub(intersectOffset), environment);
+      }
+    }
+  };
+
+  this.onMouseDown = function(event) {
+    event.preventDefault();
+
+    // Transform touch point coordinates into normalized device coordinates [-1,1]
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, this.camera);
+
+    var intersects = raycaster.intersectObjects([myPaddle]);
+    if (intersects.length > 0) {
+      draggingPaddle = true;
+
+      // Create the intersection plane to match the paddle's plane
+      intersectPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1),
+        -this.myPaddle.position.z);
+
+      // Calculate the offset (distance) from touch point (or, more accurately,
+      // the projected point on the 'screen' plane) to paddle origin
+      if (raycaster.ray.intersectPlane(intersectPlane, intersectPoint)) {
+        intersectOffset.copy(intersectPoint).sub(this.myPaddle.position);
+      }
+    }
+  };
+
+  this.onMouseUp = function(event) {
+    draggingPaddle = false;
+  };
+
+  this.camera = camera;
+  this.myPaddle = myPaddle;
+
+  renderer.domElement.addEventListener('mousemove', this.onMouseMove.bind(this), false);
+  renderer.domElement.addEventListener('mousedown', this.onMouseDown.bind(this), false);
+  renderer.domElement.addEventListener('mouseup', this.onMouseUp.bind(this), false);
+};
+
+APP.Input.constructor = APP.Input;
--- client/html/js/Main.js
+++ client/html/js/Main.js
@@ -14,41 +14,9 @@ var OpponentPaddleMaxSpeed = 0.2; // m/s
 
 // Threejs (graphics) scene objects
 var camera, scene, renderer, environment, myPaddle, opponentPaddle, ball;
-
-// Physics objects
-// var world;
+var input;
 var physics;
-// var lastTickTime;
-
-// Paddle moving / raycasting
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-var intersectPlane; // Plane of intersect (= at myPaddle's z) for pointer events
-var intersectPoint = new THREE.Vector3();
-var intersectOffset = new THREE.Vector3();
-var draggingPaddle = false;
 var opponentIntersectPlane;
-
-
-
-
-// function initPhysics() {
-//   world = new CANNON.World();
-//   world.gravity.set(0, PhysicsGravity, 0);
-//   world.addEventListener('postStep', onWorldPostStep);
-//
-//   // Add physics bodies to the simulation
-//   world.addBody(ball.physicsBody);
-//   world.addBody(environment.physicsBody);
-//   world.addBody(myPaddle.physicsBody);
-//   world.addBody(opponentPaddle.physicsBody);
-//
-//   // Listen to collisions between the ball and other bodies
-// //  ball.physicsBody.addEventListener('collide', onCollision);
-//
-//   //TODO remove - use for testing env physics
-//   ball.physicsBody.applyImpulse(new CANNON.Vec3(0, 0, 0.8), ball.physicsBody.position);
-// }
 
 function init() {
   scene = new THREE.Scene();
@@ -106,56 +74,11 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  // Attach input handler(s)
+  input = new APP.Input(renderer, camera, myPaddle);
+
   // Add event listeners
-  renderer.domElement.addEventListener('mousemove', onMouseMove, false);
-  renderer.domElement.addEventListener('mousedown', onMouseDown, false);
-  renderer.domElement.addEventListener('mouseup', onMouseUp, false);
   window.addEventListener('resize', onWindowResize, false);
-}
-
-function onMouseMove(event) {
-  event.preventDefault();
-
-  if (draggingPaddle) {
-    // Transform touch point coordinates into normalized device coordinates [-1,1]
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    raycaster.setFromCamera(mouse, camera);
-
-    if (raycaster.ray.intersectPlane(intersectPlane, intersectPoint)) {
-      myPaddle.moveTo(intersectPoint.sub(intersectOffset), environment);
-    }
-  }
-}
-
-function onMouseDown(event) {
-  event.preventDefault();
-
-  // Transform touch point coordinates into normalized device coordinates [-1,1]
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-
-  var intersects = raycaster.intersectObjects([myPaddle]);
-  if (intersects.length > 0) {
-    draggingPaddle = true;
-
-    // Create the intersection plane to match the paddle's plane
-    intersectPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1),
-      -myPaddle.position.z);
-
-    // Calculate the offset (distance) from touch point (or, more accurately,
-    // the projected point on the 'screen' plane) to paddle origin
-    if (raycaster.ray.intersectPlane(intersectPlane, intersectPoint)) {
-      intersectOffset.copy(intersectPoint).sub(myPaddle.position);
-    }
-  }
-}
-
-function onMouseUp(event) {
-  draggingPaddle = false;
 }
 
 function onWindowResize() {
