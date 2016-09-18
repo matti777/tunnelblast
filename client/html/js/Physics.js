4565c96618924d50fec301ae55de47fbd364d7bf
Matti Dahlbom <matti.dahlbom@qvik.fi>
2016-09-18 13:32:27 +0300

    Ball bounce angle adjustion; proper calculation of the opponent interseection point.

--- client/html/js/Physics.js
+++ client/html/js/Physics.js
@@ -5,65 +5,34 @@ var APP = APP || {};
 var PhysicsFixedTimeStep = 1.0 / 60.0; // Seconds. Do not change.
 var PhysicsMaxSubSteps = 3; // Do not change.
 var BallMinZSpeed = 1; // The ball must move at least this fast in z-dir
+var BallMaxZAngle = (25 / 180) * Math.PI; // Radians
 var OpponentPaddleMaxSpeed = 0.2; // m/s
+var PositiveZAxis = new CANNON.Vec3(0, 0, 1);
+var NegativeZAxis = new CANNON.Vec3(0, 0, -1);
 
 APP.Physics = function(gravity, ball, myPaddle, opponentPaddle, environment) {
+  /**
+   * Checks that the ball's velocity vector differs no more than
+   * BallMaxZAngle from the given vector (Z- / Z+ axes), and if it does,
+   * adjusts the vector.
+   */
+  this.adjustBallVelocityVector = function(axis) {
+    var ballv = this.ball.physicsBody.velocity;
 
-  //TODO move this to Physics.js?
-// function onCollision(event) {
-//   console.log('Ball collision, velocity', ball.physicsBody.velocity.z);
-//
-//   //TODO since the velocity only updates AFTER the step (thus outside this callback),
-//   // we must store event.contact (eg.) and process it after the physics step has finished!
-//
-//   var myPaddleId = myPaddle.physicsBody.id;
-//   var opponentPaddleId = opponentPaddle.physicsBody.id;
-//
-//   if (event.body.id === myPaddleId) {
-//     var contact = event.contact;
-//
-//     // console.log('hit my paddle!', event);
-//     var r = (contact.bi.id == myPaddleId) ? contact.ri : contact.rj;
-//
-//     var worldR = myPaddle.physicsBody.position.vadd(r);
-//     var localR = myPaddle.physicsBody.pointToLocalFrame(worldR);
-//     //console.log('localR', localR);
-//     //TODO adjust ball velocity using localR (discard z)
-//
-//     // Make sure the ball has at least some z velocity
-//     // console.log('velocity', ball.physicsBody.velocity);
-//
-//     if ((ball.physicsBody.velocity.z >= 0) &&
-//       (ball.physicsBody.velocity.z < BallMinZSpeed)) {
-//       console.log('myPaddle adjusted ball velocity');
-//       ball.physicsBody.velocity.z = BallMinZSpeed;
-//     }
-//   } else if ((APP.gameType === APP.GameTypes.SinglePlayer) &&
-//     (event.body.id === opponentPaddleId)) {
-//     // We only handle physics for the opponent paddle when in singleplayer mode
-//     console.log('Ball hit opponent paddle');
-//     // console.log('velocity', ball.physicsBody.velocity);
-//
-//     //TODO check same stuff as with myPaddle above
-//   }
-//
-//   // If hit anything else then opponent's paddle while in sinpleplayer mode,
-//   // update the computer player's prediction of where the ball will hit
-//   if ((APP.gameType === APP.GameTypes.SinglePlayer) &&
-//     (event.body.id !== opponentPaddleId)) {
-//    console.log('Correcting hit estimate..');
-//     // Get the current velocity vector and use it to make direction Ray
-//     var direction = new THREE.Vector3(ball.physicsBody.velocity).normalize();
-//     var ray = new THREE.Ray(ball.physicsBody.position, direction);
-//
-//     var intersectPoint = ray.intersectPlane(opponentIntersectPlane);
-//     if (intersectPoint) {
-//       console.log('new intersectPoint is', intersectPoint);
-//       //TODO update the computer opponents velocity = moving direction * OpponentPaddleMaxSpeed
-//
-//     }
-//   }
-// }
+    // Calculate the current angle
+    var unitv = ballv.unit();
+    var origAngle = Math.acos(axis.dot(unitv));
+
+    if ((origAngle < (Math.PI / 2)) && (origAngle > BallMaxZAngle)) {
+      // Ok ball is moving towards opponent but at too steep an angle; adjust
+      // by rotating the velocity towards the axis vector by angle difference
+      var angleDifference = origAngle - BallMaxZAngle;
+      var rotationAxis = unitv.cross(axis);
+      var quaternion = new CANNON.Quaternion();
+      quaternion.setFromAxisAngle(rotationAxis, angleDifference);
+      var newVelocity = quaternion.vmult(ballv, this.ball.physicsBody.velocity);
+    }
+  };
 
   /**
    Processes a contact with the ball and another body.
@@ -74,22 +43,18 @@ APP.Physics = function(gravity, ball, myPaddle, opponentPaddle, environment) {
    */
   this.processContact = function(body, localR) {
     var ballv = this.ball.physicsBody.velocity;
-    console.log('ball velocity', ballv);
+    var ballp = this.ball.physicsBody.position;
+
+    // console.log('ball velocity', ballv);
 
     if (body.id == this.myPaddle.physicsBody.id) {
       // Adjust ball bounce angle slightly by the contact point
-      var relX = localR.x / (this.myPaddle.Width / 2);
-      ballv.x += relX;
-      var relY = localR.y / (this.myPaddle.Width / 2);
-      ballv.y += relY;
+      var multiplier = 0.4;
+      ballv.x += ((localR.x / (this.myPaddle.Width / 2)) * multiplier);
+      ballv.y += ((localR.y / (this.myPaddle.Height / 2)) * multiplier);
 
-      // Make sure the ball's z velocity is at least BallMinZSpeed
-      //TODO instead of doing this, rotate the vector towards Z- axis so that its length is preserved
-      if ((ball.physicsBody.velocity.z < 0) &&
-        (ball.physicsBody.velocity.z > -BallMinZSpeed)) {
-        console.log('myPaddle adjusted ball velocity');
-        ball.physicsBody.velocity.z = -BallMinZSpeed;
-      }
+      // Make sure the ball's velocity is not at too steep angle
+      this.adjustBallVelocityVector(NegativeZAxis);
     }
 
     // If hit anything else then opponent's paddle while in sinpleplayer mode,
@@ -97,10 +62,7 @@ APP.Physics = function(gravity, ball, myPaddle, opponentPaddle, environment) {
     if ((APP.gameType === APP.GameTypes.SinglePlayer) &&
       (body.id !== this.opponentPaddle.physicsBody.id)) {
       console.log('Correcting hit estimate..');
-      // Get the current velocity vector and use it to make direction Ray
-      var direction = new THREE.Vector3(this.ball.physicsBody.velocity).normalize();
-      var ray = new THREE.Ray(this.ball.physicsBody.position, direction);
-
+      var ray = new THREE.Ray(ballp, ballv.unit());
       var intersectPoint = ray.intersectPlane(this.opponentIntersectPlane);
       if (intersectPoint) {
         console.log('new intersectPoint is', intersectPoint);
@@ -173,7 +135,7 @@ APP.Physics = function(gravity, ball, myPaddle, opponentPaddle, environment) {
 
     // Create an intersect plane for computer opponent to guess where the ball
     // is coming. Make it match the opponent paddle's z depth.
-    this.opponentIntersectPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1),
+    this.opponentIntersectPlane = new THREE.Plane(PositiveZAxis,
       -opponentPaddle.position.z);
   };
 
